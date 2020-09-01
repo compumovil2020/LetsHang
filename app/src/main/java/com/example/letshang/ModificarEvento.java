556fd983ba4ae7702cfb4d4e604a44e388b2beb6
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-09-01 18:03:14 -0500

    Fixed buttons on create event screen

---
+++ app/src/main/java/com/example/letshang/ModificarEvento.java
@@ -0,0 +1,14 @@
+package com.example.letshang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModificarEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_evento);
+    }
+}
