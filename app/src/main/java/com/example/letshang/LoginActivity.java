b74fbe527fbedaf56eb0cf078194830969fa0215
Camilo Serrano <camiloserrano@javeriana.edu.co>
2020-09-01 22:13:15 -0500

    Added navegability for some screens (principal, inscribir evento, descripcion evento, filters, map)

--- app/src/main/java/com/example/letshang/LoginActivity.java
+++ app/src/main/java/com/example/letshang/LoginActivity.java
@@ -2,13 +2,55 @@ package com.example.letshang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    Button buttonIniciar, buttonForgotPassword, buttonAtras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonIniciar = findViewById(R.id.signinBtn);
+        buttonForgotPassword = findViewById(R.id.forgotBtn);
+        buttonAtras = findViewById(R.id.backBtnLogin);
+
+
+        buttonIniciar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext() , PrincipalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonForgotPassword.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast t = Toast.makeText(getApplicationContext() , "Forgot password", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        });
+
+        buttonAtras.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/letshang/PrincipalActivity.java
+++ app/src/main/java/com/example/letshang/PrincipalActivity.java
@@ -1,14 +1,60 @@
 package com.example.letshang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    ImageView filterImage;
+    ConstraintLayout eventLayout;
+    Switch listMapSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        filterImage = findViewById(R.id.principalFilterIcon);
+        eventLayout = findViewById(R.id.eventLayout);
+        listMapSwitch = findViewById(R.id.switch1);
+
+        listMapSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Intent intent = new Intent(buttonView.getContext() , MapActivity.class);
+                startActivity(intent);
+                listMapSwitch.setChecked(false);
+            }
+
+
+        });
+
+        filterImage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext() , FiltersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        eventLayout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext() , DescripcionEvento.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
