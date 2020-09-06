9d83e3c40cb83a16ceef6d7190499178cb063011
Camilo Serrano <camiloserrano@javeriana.edu.co>
2020-09-06 17:07:50 -0500

    menu in principal activity showing options. Not navigating to other screens.

--- app/src/main/java/com/example/letshang/PrincipalActivity.java
+++ app/src/main/java/com/example/letshang/PrincipalActivity.java
@@ -1,22 +1,35 @@
 package com.example.letshang;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.android.material.navigation.NavigationView;
+
+import javax.xml.datatype.Duration;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     ImageView filterImage;
     ConstraintLayout eventLayout;
     Switch listMapSwitch;
+    ActionBarDrawerToggle menuToggle;
+    DrawerLayout drawerLayout;
+    NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,10 @@ public class PrincipalActivity extends AppCompatActivity {
         filterImage = findViewById(R.id.principalFilterIcon);
         eventLayout = findViewById(R.id.eventLayout);
         listMapSwitch = findViewById(R.id.switch1);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.principal_nav_view);
+
+        setupMenu();
 
         listMapSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -56,5 +73,41 @@ public class PrincipalActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    void setupMenu(){
+
+        menuToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                R.string.menu_open_reader,
+                R.string.menu_close_reader);
+
+        drawerLayout.setDrawerListener(menuToggle);
+        menuToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(getApplicationContext(), item.getTitle() ,Toast.LENGTH_SHORT ).show();
+                return false;
+            }
+        });
+    }
+
+
+    // needed to setup menu toggle button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (menuToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
