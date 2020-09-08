da02e5efc78d4b5f50868a25950bcc2c6c2cf55b
Camilo Serrano <camiloserrano@javeriana.edu.co>
2020-09-07 21:21:26 -0500

    Minor fixes

--- app/src/main/java/com/example/letshang/InscribirEvento.java
+++ app/src/main/java/com/example/letshang/InscribirEvento.java
@@ -9,24 +9,14 @@ import android.widget.Button;
 public class InscribirEvento extends AppCompatActivity {
 
 
-    Button backButton, registerButton;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscribir_evento);
 
-        backButton = findViewById(R.id.enrollBack);
         registerButton = findViewById(R.id.registerButton);
-
-
-        backButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
 
         registerButton.setOnClickListener(new View.OnClickListener(){
 
