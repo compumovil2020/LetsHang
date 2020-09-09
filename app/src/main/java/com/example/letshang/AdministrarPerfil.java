09ac3ed8f30729b7e0f5fe002049f5ba140771c1
Camilo Serrano <camiloserrano@javeriana.edu.co>
2020-09-09 09:45:44 -0500

    Hot fixex

--- app/src/main/java/com/example/letshang/AdministrarPerfil.java
+++ app/src/main/java/com/example/letshang/AdministrarPerfil.java
@@ -14,9 +14,6 @@ import com.google.android.material.navigation.NavigationView;
 
 public class AdministrarPerfil extends AppCompatActivity {
 
-    ActionBarDrawerToggle menuToggle;
-    DrawerLayout drawerLayout;
-    NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,67 +23,8 @@ public class AdministrarPerfil extends AppCompatActivity {
         getSupportActionBar().setTitle("Mi perfil");
 
 
-        drawerLayout = findViewById(R.id.perfil_drawer_layout);
-        //navView = findViewById(R.id.perfil_nav_view);
 
-        //setupMenu();
-    }
-
-    void setupMenu(){
-
-        menuToggle = new ActionBarDrawerToggle(this,
-                drawerLayout,
-                R.string.menu_open_reader,
-                R.string.menu_close_reader);
-
-        drawerLayout.setDrawerListener(menuToggle);
-        menuToggle.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-
-        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() ==  R.id.item_menu_administrar_perfil){
-                    //does nothing
-                }
-                if(item.getItemId() ==  R.id.item_menu_sugerir){
-                    Intent intent = new Intent(getApplicationContext() , CrearSugerencia.class);
-                    startActivity(intent);
-                }
-                if(item.getItemId() ==  R.id.item_menu_eventos){
-                    Intent intent = new Intent(getApplicationContext() , PrincipalActivity.class);
-                    startActivity(intent);
-                }
-                if(item.getItemId() ==  R.id.item_menu_eventos_inscritos){
-                    Intent intent = new Intent(getApplicationContext() , EventosInscritos.class);
-                    startActivity(intent);
-                }
-                if(item.getItemId() ==  R.id.item_menu_logout){
-                    Intent intent = new Intent(getApplicationContext() , LoginActivity.class);
-                    startActivity(intent);
-                }
-                if(item.getItemId() ==  R.id.item_menu_pago){
-                    Toast.makeText(getApplicationContext(), item.getTitle() ,Toast.LENGTH_SHORT ).show();
-
-                }
-                return true;
-            }
-        });
     }
 
 
-    // needed to setup menu toggle button
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        if (menuToggle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
