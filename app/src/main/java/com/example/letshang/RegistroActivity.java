3ef0e837209b6dc99a0e50c699d8c5a64cacf5da
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-09-08 21:21:03 -0500

    Fixed navegability of register screens

--- app/src/main/java/com/example/letshang/RegistroActivity.java
+++ app/src/main/java/com/example/letshang/RegistroActivity.java
@@ -2,13 +2,29 @@ package com.example.letshang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    Button btnSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        btnSiguiente = findViewById(R.id.signupBtn);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RegistroAdicional.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
