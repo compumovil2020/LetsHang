909ce82b8416125b2e45c6b8bbaa86e3ec4b1e23
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-09-29 11:49:21 -0500

    Refactored ids in RegistroActivity

--- app/src/main/java/com/example/letshang/activities/RegistroActivity.java
+++ app/src/main/java/com/example/letshang/activities/RegistroActivity.java
@@ -6,25 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
 import com.example.letshang.R;
 
 public class RegistroActivity extends AppCompatActivity {
 
-    private Button btnSiguiente;
+    private EditText etNombre, etCorreo, etTelefono, etPassword, etVerify, etNuevo;
+    private ToggleButton tbDeportes, tbJuegosMesa, tbConciertos, tbFiesta, tbCharlar, tbConferencias;
+    private Button btnSiguiente, btnAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        btnSiguiente = findViewById(R.id.signupBtn);
+        etNombre = findViewById(R.id.etNombreRegistro);
+        etCorreo = findViewById(R.id.etCorreoRegistro);
+        etTelefono = findViewById(R.id.etTelefonoRegistro);
+        etPassword = findViewById(R.id.etPasswordRegistro);
+        etVerify = findViewById(R.id.etVerifyRegistro);
+        etNuevo = findViewById(R.id.etNuevoRegistro);
+
+        tbDeportes = findViewById(R.id.tbDeportesRegistro);
+        tbJuegosMesa = findViewById(R.id.tbJuegosMesaRegistro);
+        tbConciertos = findViewById(R.id.tbConciertosRegistro);
+        tbFiesta = findViewById(R.id.tbFiestasRegistro);
+        tbCharlar = findViewById(R.id.tbCharlarRegistro);
+        tbConferencias = findViewById(R.id.tbConferenciasRegistro);
+
+        btnSiguiente = findViewById(R.id.btnSiguienteRegistro);
+        btnAgregar = findViewById(R.id.btnAgregarRegistro);
 
         btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), RegistroAdicional.class);
                 startActivity(intent);
+            }
+        });
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
