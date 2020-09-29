ca22d5d2d02e7274fba4d4904453107d5ae899de
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-09-29 12:01:56 -0500

    Refactored ids in RegistroAdicionalActivity

--- app/src/main/java/com/example/letshang/activities/RegistroActivity.java
+++ app/src/main/java/com/example/letshang/activities/RegistroActivity.java
@@ -42,7 +42,7 @@ public class RegistroActivity extends AppCompatActivity {
         btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), RegistroAdicional.class);
+                Intent intent = new Intent(view.getContext(), RegistroAdicionalActivity.class);
                 startActivity(intent);
             }
         });
