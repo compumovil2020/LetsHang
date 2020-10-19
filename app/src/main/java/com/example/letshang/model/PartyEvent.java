f769683d3bfcaf088b6205683ff50a5a1254a059
Santiago Caro Duque <santiagocaroduque@gmail.com>
2020-10-19 17:04:25 -0500

    Added constructor PartyEvent #87

--- app/src/main/java/com/example/letshang/model/PartyEvent.java
+++ app/src/main/java/com/example/letshang/model/PartyEvent.java
@@ -1,4 +1,21 @@
 package com.example.letshang.model;
 
-public class PartyEvent {
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class PartyEvent extends Event {
+
+    /**
+     * The music genre that will be played at the party
+     */
+    private String musicGenre;
+
+
+    public PartyEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location, String musicGenre) {
+        super(title, description, startDate, endDate, price, maximumCapacity, tags, location);
+        this.musicGenre = musicGenre;
+        this.type = EventsEnum.PARTY;
+    }
 }
