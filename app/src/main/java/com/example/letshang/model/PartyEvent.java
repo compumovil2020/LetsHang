c6e4b12f6b098383964a5069274d66a959f58d10
Santiago Caro <santiagocaroduque@gmail.com>
2020-10-20 13:06:01 -0500

    Added attributes, new constructor, added setters #87 #104

--- app/src/main/java/com/example/letshang/model/PartyEvent.java
+++ app/src/main/java/com/example/letshang/model/PartyEvent.java
@@ -11,11 +11,43 @@ public class PartyEvent extends Event {
      * The music genre that will be played at the party
      */
     private String musicGenre;
+    private String theme;
+    private int minimumAge;
+    private String rules;
 
 
-    public PartyEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location, String musicGenre) {
+    /**
+     * General Constructor from abstract Event
+     */
+    public PartyEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location) {
+        super(title, description, startDate, endDate, price, maximumCapacity, tags, location);
+
+    }
+
+    /**
+     * Specific Constructor from Party Event
+     */
+    public PartyEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location, String musicGenre, String theme, int minimumAge, String rules) {
         super(title, description, startDate, endDate, price, maximumCapacity, tags, location);
         this.musicGenre = musicGenre;
-        this.type = EventsEnum.PARTY;
+        this.theme = theme;
+        this.minimumAge = minimumAge;
+        this.rules = rules;
+    }
+
+    public void setMusicGenre(String musicGenre) {
+        this.musicGenre = musicGenre;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public void setMinimumAge(int minimumAge) {
+        this.minimumAge = minimumAge;
+    }
+
+    public void setRules(String rules) {
+        this.rules = rules;
     }
 }
