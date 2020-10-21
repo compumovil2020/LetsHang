6ffe5ed9e519fc6087a47e8f5fc8f49174eac7e5
Nicolas Lopez <nlopez97f@gmail.com>
2020-10-20 21:54:46 -0500

    ActivityPerfil ya agrega los tags de preferencias

--- app/src/main/java/com/example/letshang/activities/AdministrarPerfilActivity.java
+++ app/src/main/java/com/example/letshang/activities/AdministrarPerfilActivity.java
@@ -3,18 +3,26 @@ package com.example.letshang.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.letshang.R;
 import com.example.letshang.model.Participant;
+import com.example.letshang.model.Preference;
 import com.example.letshang.model.User;
 import com.example.letshang.providers.UserProvider;
 import com.google.android.material.navigation.NavigationView;
@@ -32,6 +40,10 @@ public class AdministrarPerfilActivity extends AppCompatActivity {
     private UserProvider usrProvider;
     private Participant part;
 
+    private LinearLayout linearLayoutContenedor;
+    private LinearLayout linearLayoutTextos;
+    private TextView textViewTags;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +57,8 @@ public class AdministrarPerfilActivity extends AppCompatActivity {
 
         btnGuardar = findViewById(R.id.btnGuardarAdministrarPerfil);
 
+        linearLayoutContenedor = findViewById(R.id.llAdministrarPerfil3);
+
         getSupportActionBar().setTitle("Mi perfil");
 
         usrProvider = new UserProvider();
@@ -57,7 +71,42 @@ public class AdministrarPerfilActivity extends AppCompatActivity {
         String today = formatter.format(part.getBirthDate());
         etFecha.setText(today);
 
+        Preference pr = part.getPreferences();
 
+        String[] tags = pr.getInterests();
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+               LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+
+        int contadorTags = 0;
+        for(String tag : tags){
+            if(contadorTags == 0){
+                linearLayoutTextos = new LinearLayout(this);
+                linearLayoutTextos.setOrientation(LinearLayout.HORIZONTAL);
+                linearLayoutTextos.setLayoutParams(layoutParams);
+                linearLayoutContenedor.addView(linearLayoutTextos);
+            }
+
+            if(contadorTags < 3){
+                TableRow.LayoutParams lp = new TableRow.LayoutParams();
+                lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                lp.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+                lp.weight = 1;
+                Typeface face = ResourcesCompat.getFont(this,R.font.coves_bold);
+                textViewTags = new TextView(this);
+                textViewTags.setLayoutParams(lp);
+                textViewTags.setGravity(Gravity.CENTER);
+                textViewTags.setTextSize(16);
+                textViewTags.setText(tag);
+
+                linearLayoutTextos.addView(textViewTags);
+            }
+
+            if(contadorTags==2){
+                contadorTags = 0;
+            }
+            contadorTags++;
+        }
 
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
