a5f58840124a39fe6f3cc3e4c2fb025bb6706ce7
Santiago Caro <santiagocaroduque@gmail.com>
2020-10-21 11:23:12 -0500

    Refactor PartyEvent to Social event #87 #104

--- app/src/main/java/com/example/letshang/model/SocialEvent.java
+++ app/src/main/java/com/example/letshang/model/SocialEvent.java
@@ -5,21 +5,30 @@ import com.google.android.gms.maps.model.LatLng;
 import java.util.Collection;
 import java.util.Date;
 
-public class PartyEvent extends Event {
+public class SocialEvent extends Event {
 
     /**
      * The music genre that will be played at the party
      */
     private String musicGenre;
+    /**
+     * Theme of the social event.
+     */
     private String theme;
+    /**
+     * Minimum Age of acceptance of the event
+     */
     private int minimumAge;
+    /**
+     * Important Rules for the social event.
+     */
     private String rules;
 
 
     /**
      * General Constructor from abstract Event
      */
-    public PartyEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location) {
+    public SocialEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location) {
         super(title, description, startDate, endDate, price, maximumCapacity, tags, location);
 
     }
@@ -27,7 +36,7 @@ public class PartyEvent extends Event {
     /**
      * Specific Constructor from Party Event
      */
-    public PartyEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location, String musicGenre, String theme, int minimumAge, String rules) {
+    public SocialEvent(String title, String description, Date startDate, Date endDate, long price, int maximumCapacity, Collection<String> tags, LatLng location, String musicGenre, String theme, int minimumAge, String rules) {
         super(title, description, startDate, endDate, price, maximumCapacity, tags, location);
         this.musicGenre = musicGenre;
         this.theme = theme;
