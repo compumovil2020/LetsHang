d0b67e24e4e607647279bf613f588138fc4e05d3
camiloserr <camiloserrano@javeriana.edu.co>
2020-10-21 13:09:57 -0500

    User provider and event provider working with jardcoded data(not DB). Both providers are singleton. Example of how to use them in unit tests.

---
+++ app/src/test/java/com/example/letshang/ProvidersUnitTest.java
@@ -0,0 +1,53 @@
+package com.example.letshang;
+
+import com.example.letshang.model.Event;
+import com.example.letshang.model.Host;
+import com.example.letshang.model.SportEvent;
+import com.example.letshang.model.User;
+import com.example.letshang.providers.EventProvider;
+import com.example.letshang.providers.UserProvider;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.junit.Assert.*;
+
+public class ProvidersUnitTest {
+
+    // esto es porque me da pereza crear un evento entonces solo creo un mock
+    @Mock
+    SportEvent mockEvent;
+
+    @Mock
+    Host mockHost;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+
+    @Test
+    public void UserProviderShouldReturnCurrentUser(){
+        UserProvider up = UserProvider.getInsatance();
+
+        User u = up.getCurrentUser();
+        assertNotNull(u);
+    }
+
+    @Test
+    public void EventProviderShouldAddEvent(){
+        EventProvider ep = EventProvider.getInsatance();
+
+
+       ep.createEvent(mockEvent, mockHost);
+
+       int ID = mockEvent.getID();
+
+       Event e = ep.getEventByID(ID);
+
+       assertEquals(e,mockEvent);
+
+    }
+}
