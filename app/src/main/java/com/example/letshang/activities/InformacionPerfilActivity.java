02c0db4b73b9a80b4cd909f53c993695209e7f51
Juls <julio.mejia@javeriana.edu.co>
2020-10-23 23:04:30 -0500

    Revert "Colors have been modified, also the information profile layout had been modified, in order to be a few responsive." Few errors fixed

--- app/src/main/java/com/example/letshang/activities/InformacionPerfilActivity.java
+++ app/src/main/java/com/example/letshang/activities/InformacionPerfilActivity.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TableRow;
@@ -40,6 +41,7 @@ public class InformacionPerfilActivity extends AppCompatActivity {
     private NavigationView navView;
     private FirebaseAuth mAuth;
     private TextView tvNombre, tvCorreo;
+    private ImageButton btnFacebook, btnInstagram, btnlinkedin;
 
     private UserProvider usProv = UserProvider.getInsatance();
     private LinearLayout linearLayoutContenedor;
@@ -67,6 +69,9 @@ public class InformacionPerfilActivity extends AppCompatActivity {
 
         listViewEvents = findViewById(R.id.listEventosInformacionPerfil);
 
+        btnFacebook = findViewById(R.id.btnFacebookInformacionPerfil);
+        btnInstagram = findViewById(R.id.btnInstagramInformacionPerfil);
+        btnlinkedin = findViewById(R.id.btnLinkedinInformacionPerfil);
 
         Participant parti = (Participant)usProv.getCurrentUser();
 
