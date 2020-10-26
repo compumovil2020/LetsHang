44ccb9dfc7a6f2b308f6e32fed682bcc34cda460
Nicolas Lopez <nlopez97f@gmail.com>
2020-10-26 00:28:22 -0500

    Layout Game Event created

---
+++ app/src/main/java/com/example/letshang/CreateGameEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.letshang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateGameEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_game_event);
+    }
+}
