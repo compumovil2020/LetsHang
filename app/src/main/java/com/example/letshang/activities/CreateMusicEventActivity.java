4d44c73d303489a491bc0f63eccd6bdd3098128e
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-10-25 20:13:21 -0500

    MusicEvent Layout finished

---
+++ app/src/main/java/com/example/letshang/activities/CreateMusicEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.letshang.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.letshang.R;
+
+public class CreateMusicEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_music_event);
+    }
+}
