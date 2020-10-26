7e9360ead661c1fcddb3590af706350bd1212a4c
Juls <julio.mejia@javeriana.edu.co>
2020-10-25 22:59:54 -0500

    Listo para pull

--- app/src/main/java/com/example/letshang/activities/CrearSugerenciaActivity.java
+++ app/src/main/java/com/example/letshang/activities/CrearSugerenciaActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
