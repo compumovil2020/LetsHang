795efbafa5ce07f1433399f2cdd7e06a64fe26d6
Santiago Caro Duque <santiagocaroduque@gmail.com>
2020-10-26 22:32:32 -0500

    Added Create Social event #112

--- app/src/main/java/com/example/letshang/model/SocialEvent.java
+++ app/src/main/java/com/example/letshang/model/SocialEvent.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class SocialEvent extends Event {
 
     /**
-     * The music genre that will be played at the party
+     * The music genre that will be played at the event
      */
     private String musicGenre;
     /**
